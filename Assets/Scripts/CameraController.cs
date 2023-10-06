0656edf9557467c5a9556420934ff9186633caa4
Leo Dresang <lrdresang@yahoo.com>
2023-10-06 16:31:01 -0500

    Camera (currently just follows)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float FollowSpeed = 10f;
+    private Transform target;
+    private GameObject Nightingale = null;
+
 
     // Update is called once per frame
     void Update()
     {
+        if (Nightingale == null)
+        {
+            Nightingale = GameObject.Find("Nightingale");
+        }
+
         
+
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+
+
     }
 }
