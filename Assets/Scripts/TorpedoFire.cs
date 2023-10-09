53b73f20e5940037761b1c496abc941b17e19b6a
Brodbeatz <brodbeckm8002@my.uwstout.edu>
2023-10-09 16:06:06 -0500

    Gave collisions to walls and torpedos

--- Assets/Scripts/TorpedoFire.cs
+++ Assets/Scripts/TorpedoFire.cs
@@ -8,22 +8,51 @@ public class TorpedoFire : MonoBehaviour
     private float fireSpeed = 4.0f;
     private float leftBound = -10;
     private float rightBound = 10;
-    private NightingaleMovement movementScript;
+    private GameObject Nightingale = null;
+    private Vector2 direction;
 
     
     // Start is called before the first frame update
     void Start()
     {
-       
+       if(Nightingale == null)
+        {
+            Nightingale = GameObject.Find("Nightingale");
+        }
+
+        if (Nightingale.GetComponent<NightingaleMovement>().getIsFacingRight())
+        {
+            direction = Vector2.left;
+        }
+
+        else
+        {
+            direction = Vector2.right;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        transform.Translate(Vector2.right * Time.deltaTime * fireSpeed);
 
-        if(transform.position.x < leftBound || transform.position.x > rightBound)
+        if (Nightingale == null)
+        {
+            Nightingale = GameObject.Find("Nightingale");
+        }
+
+        /*if (Nightingale.GetComponent<NightingaleMovement>().getIsFacingRight())
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * fireSpeed);
+        }
+
+        else
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * fireSpeed);
+        }*/
+
+        transform.Translate(direction * Time.deltaTime * fireSpeed);
+
+        if (transform.position.x < leftBound || transform.position.x > rightBound)
         {
             Destroy(gameObject);
         }
