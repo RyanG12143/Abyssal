06574ac28a110830b848ea84504a8289b35bf8be
Rookioo <andersond5235@my.uwstout.edu>
2023-10-11 16:29:39 -0500

    Seamonkey Prototype Finished
    Added final prototype methods for Seamonkey class

--- Assets/Scripts/Creature/CreatureFollow.cs
+++ Assets/Scripts/Creature/CreatureFollow.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public enum EnemyState
 {
     Wander,
     Follow,
+    Run,
     Die,
 }
 
@@ -19,6 +21,10 @@ public class EnemyController : MonoBehaviour
 
     public float range = 2f;
     public float moveSpeed = 2f;
+
+    public bool hitPlayer = false;
+    private bool isFacingRight = true;
+    public float moveDistance = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -39,22 +45,40 @@ public class EnemyController : MonoBehaviour
             case EnemyState.Follow:
                 Follow();
                 break;
+            case EnemyState.Run:
+                Run();
+                break;
             case EnemyState.Die:
                 // Die();
                 break;
         }
 
         // Checks what state to be in
-        if (IsPlayerInRange(range) && currState != EnemyState.Die)
+        if (IsPlayerInRange(range) && currState != EnemyState.Die && hitPlayer == true)
+        {
+            currState = EnemyState.Run;
+        }
+        else if (IsPlayerInRange(range) && currState != EnemyState.Die && currState != EnemyState.Run)
         {
             currState = EnemyState.Follow;
         }
         else if (!IsPlayerInRange(range) && currState != EnemyState.Die)
         {
             currState = EnemyState.Wander;
+
         }
+
+        // Checking enemy hit player
+        if (Vector3.Distance(transform.position, player.transform.position) <= 0.7)
+        {
+            hitPlayer = true;
+        }
+
+        // Flipping sprite
+        //flip();
     }
 
+    // Checking if player is in range
     private bool IsPlayerInRange(float range)
     {
         return Vector3.Distance(transform.position, player.transform.position) <= range;
@@ -63,7 +87,7 @@ public class EnemyController : MonoBehaviour
     // Wander method
     void Wander()
     {
-        if (isFacingRight())
+        if (isFacingRight)
         {
             myRigidbody.velocity = new Vector2(moveSpeed, 0f);
         }
@@ -92,8 +116,35 @@ public class EnemyController : MonoBehaviour
         myRigidbody.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
     }
 
-    bool isFacingRight()
+    //bool isFacingRight()
+    //{
+    //    return transform.localScale.x > 0;
+    //}
+
+    // Run method
+
+    void Run()
     {
-        return transform.localScale.x > 0;
+        Vector2 targetPosition = target.position;
+        Vector2 currentPosition = transform.position;
+
+        Vector2 direction = (targetPosition - currentPosition).normalized;
+
+        // Use LookAt to make the enemy face the player
+        transform.right = direction;
+
+        myRigidbody.velocity = new Vector2(-direction.x * moveSpeed, -direction.y * moveSpeed);
     }
+
+    //private void flip()
+    //{
+    //    if ((isFacingRight && myRigidbody.velocity.x > 0.05) || (!isFacingRight && myRigidbody.velocity.y < -0.05))
+    //    {
+    //        isFacingRight = !isFacingRight;
+    //        Vector3 localeScale = transform.localScale;
+    //        localeScale.y *= -1;
+    //        transform.localScale = localeScale;
+
+    //    }
+    //}
 }
