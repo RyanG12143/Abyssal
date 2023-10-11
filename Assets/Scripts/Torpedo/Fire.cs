96b6af13e436e6fa325d8d5f681dc26391b6479b
Brodbeatz <brodbeckm8002@my.uwstout.edu>
2023-10-11 16:30:22 -0500

    Pretty much finished torpedo

--- Assets/Scripts/Torpedo/Fire.cs
+++ Assets/Scripts/Torpedo/Fire.cs
@@ -6,6 +6,7 @@ public class Fire : MonoBehaviour
 {
     public GameObject torpedoPrefab;
     public Vector2 torpedoLocation;
+    private float countdown = 4;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +17,16 @@ public class Fire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Places the torpedo on the right side of the sub
         torpedoLocation = new Vector2(transform.position.x + 0.1f, transform.position.y);
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        //countdown -= Time.deltaTime;
+
+        //Fires the torpedo if you press left shift
+        if (Input.GetKeyDown(KeyCode.LeftShift)) //&& countdown <=0)
         {
             
             Instantiate(torpedoPrefab, torpedoLocation, torpedoPrefab.transform.rotation);
+            //countdown = 4;
         }
     }
 }
---
+++ Assets/Scripts/Torpedo/TorpedoFire.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TorpedoFire : MonoBehaviour
+{
+    private float fireSpeed = 4.0f;
+    private float horizontalBound;
+    private float verticalBound;
+    private GameObject Nightingale = null;
+    private GameObject mainCamera;
+    
+    private Vector2 direction;
+
+    
+    //Initializes the torpedo's direction
+    void Start()
+    {
+       if(Nightingale == null)
+        {
+            Nightingale = GameObject.Find("Nightingale");
+        }
+
+
+        if (mainCamera == null)
+        {
+            mainCamera = GameObject.Find("Main Camera");
+        }
+
+        //Has the bounds of the screen be based on the current camera
+        horizontalBound = Mathf.Abs(mainCamera.transform.position.x) + 10;
+        verticalBound = Mathf.Abs(mainCamera.transform.position.y) + 5;
+
+
+        direction = Nightingale.GetComponent<NightingaleMovement>().getFacing();
+       
+ 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Checks if the nightingale is initializes and find it if it isn't
+        if (Nightingale == null)
+        {
+            Nightingale = GameObject.Find("Nightingale");
+        }
+
+        if (mainCamera == null)
+        {
+            mainCamera = GameObject.Find("Main Camera");
+        }
+
+
+        
+
+        transform.Translate(direction * Time.deltaTime * fireSpeed);
+
+        //Destroys the torpedo if it goes out of bounds
+        if (transform.position.x < -horizontalBound || transform.position.x > horizontalBound
+            || transform.position.y < -verticalBound || transform.position.y > verticalBound)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    
+}
