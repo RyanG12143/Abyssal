eea2057e700d71ebce7937f3e4cc64001c47eb22
Leo Dresang <lrdresang@yahoo.com>
2023-10-16 22:00:21 -0500

    Sonar

--- Assets/Scripts/Sonar.cs
+++ Assets/Scripts/Sonar.cs
@@ -12,13 +12,18 @@ public class Sonar : MonoBehaviour
     public GameObject SonarSpread;
 
     private Boolean sonarSizeIncrease;
-    
+
+    private Boolean isFadeRunning = false;
+
+    private Boolean isEffectsRunning = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
         sonarSizeIncrease = false;
         SonarSpread.transform.localScale = new Vector3(1f, 1f, 1f);
+        SonarSpread.GetComponent<SpriteRenderer>().color = new Color((SonarSpread.GetComponent<SpriteRenderer>().color.r), (SonarSpread.GetComponent<SpriteRenderer>().color.g), (SonarSpread.GetComponent<SpriteRenderer>().color.b), 0f);
         creaturesScanned = GameObject.FindGameObjectsWithTag("CreatureSonar");
     }
 
@@ -32,27 +37,33 @@ public class Sonar : MonoBehaviour
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
-        { 
-
+        {
+            SonarSpread.GetComponent<SpriteRenderer>().color = new Color((SonarSpread.GetComponent<SpriteRenderer>().color.r), (SonarSpread.GetComponent<SpriteRenderer>().color.g), (SonarSpread.GetComponent<SpriteRenderer>().color.b), 1f);
             SonarScan(creaturesScanned);
         }
 
+
+    }
+
+    private void FixedUpdate()
+    {
+
         if ((SonarSpread.transform.localScale.x < 40f) && sonarSizeIncrease)
         {
+            if (!isEffectsRunning)
+            {
+                StartCoroutine(Effects());
+            }
+
 
             SonarSpread.transform.localScale += new Vector3(14f, 14f, 0f) * Time.deltaTime;
 
             foreach (var creature in creaturesScanned)
             {
 
-                if (Vector2.Distance(SonarSpread.transform.position, creature.transform.position) < (SonarSpread.transform.localScale.x * 0.5))
+                if ((Vector2.Distance(SonarSpread.transform.position, creature.transform.position) < (SonarSpread.transform.localScale.x * 0.5)) && !isFadeRunning)
                 {
-
-                    //creature.GetComponent<SpriteRenderer>().color = Color.red;
-                    
                     StartCoroutine(Fade(creature));
-                    Debug.Log("Coroutine Called!");
-                   
                 }
             }
 
@@ -85,6 +96,9 @@ public class Sonar : MonoBehaviour
 
     IEnumerator Fade(GameObject creature)
     {
+
+        isFadeRunning = true;
+
         Color current = new Color((creature.GetComponent<SpriteRenderer>().color.r), (creature.GetComponent<SpriteRenderer>().color.g), (creature.GetComponent<SpriteRenderer>().color.b));
 
         Color scanned = Color.red;
@@ -109,7 +123,38 @@ public class Sonar : MonoBehaviour
 
         }
 
-        //  creature.GetComponent<SpriteRenderer>().color = Color.Lerp(scanned, current, Time.deltaTime);
+        isFadeRunning = false;
+
+    }
+
+    IEnumerator Effects()
+    {
+
+        isEffectsRunning = true;
+
+        Color current = new Color((SonarSpread.GetComponent<SpriteRenderer>().color.r), (SonarSpread.GetComponent<SpriteRenderer>().color.g), (SonarSpread.GetComponent<SpriteRenderer>().color.b), 1f);
+
+        Color end = new Color((SonarSpread.GetComponent<SpriteRenderer>().color.r), (SonarSpread.GetComponent<SpriteRenderer>().color.g), (SonarSpread.GetComponent<SpriteRenderer>().color.b), 0f);
+
+        for (int i = 0; i < 50; i++)
+        {
+
+            SonarSpread.GetComponent<SpriteRenderer>().color = Color.Lerp(current, end, i / 50.0f);
+
+            yield return new WaitForSeconds(.05f);
+
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        for (int i = 0; i < 1; i++)
+        {
+
+            SonarSpread.GetComponent<SpriteRenderer>().color = Color.Lerp(end, current, i / 1.0f);
+
+        }
+
+        isEffectsRunning = false;
 
     }
 
