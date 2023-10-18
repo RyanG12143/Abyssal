912beed98caa182097f09328c7fb16774487bc04
RyanG12143 <loop9981@gmail.com>
2023-10-18 15:32:58 -0500

    Revert "Deleted Excess script"
    This reverts commit a1d01f9ae7b76f97811c0d3b3fc57302185b8383.

---
+++ Assets/Scripts/WallGone.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class WallGone : MonoBehaviour
+{
+    public GameObject ByeWall;
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Torpedo")
+        Destroy(ByeWall);
+    }
+}
