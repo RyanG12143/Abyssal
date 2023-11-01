680b84ab04563350c0c47b543cb8c629347d0dd3
Brodbeatz <brodbeckm8002@my.uwstout.edu>
2023-11-01 16:29:50 -0500

    Finished water currents. Started on explosive package

--- Assets/Scripts/Currents/CurrentVertical.cs
+++ Assets/Scripts/Currents/CurrentVertical.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CurrentForce : MonoBehaviour
+public class CurrentVertical : MonoBehaviour
 {
-    private float force = 5f;
+    public float force = 5f;
 
+    /*Matthew Brodbeck 11/01/2023
+     * Forces the object in a vertical direction*/
     private void OnTriggerStay2D(Collider2D other)
     {
         other.GetComponent<Rigidbody2D>().AddForce(transform.up * force);
