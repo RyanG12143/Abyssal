ba76be9e4df7fac2814724b85b0a0c011b7b0bda
Rookioo <andersond5235@my.uwstout.edu>
2023-11-09 05:50:53 -0600

    Major Creature Update
    Monstrasquid & SwordFish implimentation

--- Assets/Scripts/Creature/Monstrosquid.cs
+++ Assets/Scripts/Creature/Monstrosquid.cs
@@ -13,32 +13,28 @@ public class Monstrosquid : MonoBehaviour
     {
         Appear,
         Idle,
-        Eat,
+        Attack,
         Leave,
     }
 
     private GameObject player;
     public Animator animator;
-    public EnemyAction currState = EnemyAction.Appear;
+    public EnemyAction currState = EnemyAction.Idle;
 
     public Transform target;
     Rigidbody2D myRigidbody;
 
     // Editable movement variables
-    public float range = 2f;
-    public float moveSpeed = 0f;
-    public float moveDistance = 5f;
-    public float turnInterval = 2.0f;
-    public float wanderBufferTime = 2.0f;
-    public float slowTimeInterval = 0.5f;
+    public float scareRange = 10f;
+    public float playerRange = 4f;
+    public float creatureRange = 4f;
 
     // Bool's for creature state changes
-    public bool hitPlayer = false;
-    public bool hitByTorpedo = false;
-    public bool creatureTurn = false;
-    public bool jumpscare = false;
-    public bool postAttack = false;
-
+    public bool grabPlayer = false;
+    public bool grabCreature = false;
+    private bool jumpscare = true;
+    private bool attacking = true;
+    private bool leaveTrack = true;
 
     // Start is called before the first frame update
     void Start()
@@ -53,50 +49,43 @@ public class Monstrosquid : MonoBehaviour
     void Update()
     {
         stateSwitch();
-        animationUpdates();
-    }
-    private void animationUpdates()
-    {
-        animator.SetBool("Attack", hitPlayer);
-        animator.SetBool("Jumpscare", jumpscare);
-        animator.SetBool("idleReturn", postAttack);
     }
 
     // Switches the states of the enemy creature
     private void stateSwitch()
     {
         // Checks what state to be in
-        //if (hitByTorpedo)
-        //{
-        //    currState = EnemyAction.Leave;
-        //}
-        //else if (IsPlayerInRange(range) && currState != EnemyAction.Die && hitPlayer == true)
-        //{
-        //    currState = EnemyAction.Run;
-        //}
-        //else if (IsPlayerInRange(range) && currState != EnemyAction.Die && currState != EnemyAction.Run)
-        //{
-        //    currState = EnemyAction.Follow;
-        //}
-        //else if (!IsPlayerInRange(range) && currState != EnemyAction.Die)
-        //{
-        //    currState = EnemyAction.Wander;
-        //}
+        if (isCreatureInRange(creatureRange))
+        {
+            currState = EnemyAction.Leave;
+        }
+        else if (isPlayerInRange(playerRange))
+        {
+            currState = EnemyAction.Attack;
+        }
+        else if (isPlayerInRange(scareRange) && jumpscare)
+        {
+            currState = EnemyAction.Appear;
+        }
+        else
+        {
+            currState = EnemyAction.Idle;
+        }
 
         // Switches current state based on currState
         switch (currState)
         {
             case EnemyAction.Appear:
-                Appear();
+                appear();
                 break;
             case EnemyAction.Idle:
-                Idle();
+                idle();
                 break;
-            case EnemyAction.Eat:
-                Eat();
+            case EnemyAction.Attack:
+                attack();
                 break;
             case EnemyAction.Leave:
-                Leave();
+                leave();
                 break;
         }
     }
@@ -105,149 +94,111 @@ public class Monstrosquid : MonoBehaviour
     // Different creature state methods
     //
     //
-
-    void Appear()
+    // Jumpskare and moving into level
+    void appear()
     {
-        //myAnimator.SetTrigger(name: "jumpScare");
-        currState = EnemyAction.Idle;
+        StartCoroutine(jumpscareActivate());
+        myRigidbody.velocity = new Vector2(0f, 4f);
     }
 
-    // Wander creature state
-    void Idle()
+    void idle()
     {
-        //StartCoroutine(wanderBuffer());
-
-        Vector3 localScale = transform.localScale;
-
-            if (transform.up.y < 0f)
-            {
-                localScale.y = 1;
-                transform.localScale = localScale;
-            }
-
-            if (creatureTurn)
-            {
-                myRigidbody.velocity = new Vector2(moveSpeed, 0f);
-            }
-            else
-            {
-                myRigidbody.velocity = new Vector2(-moveSpeed, 0f);
-            }
 
     }
 
-    void Eat()
+    void attack()
     {
-        
+        StartCoroutine(attackActivate());
     }
 
-    // Die method
-    void Leave()
+    void leave()
     {
-      
+        grabCreature = true;
+        StartCoroutine(Leaving());
     }
 
-    // Helper methods
-    //
-    //
-    // Flipping sprite at critical points (Looking Straight Up and Down)
-    //private void flip()
-    //{
-    //    Vector2 targetPosition = target.position;
-    //    Vector2 currentPosition = transform.position;
-
-    //    Vector2 direction = (targetPosition - currentPosition).normalized;
-
-    //    if ((isFacingRight && direction.x < -0.01) || (!isFacingRight && direction.x > 0.01))
-    //    {
-    //        isFacingRight = !isFacingRight;
-
-    //        Vector3 localScale = transform.localScale;
-    //        localScale.y *= -1;
-    //        transform.localScale = localScale;
-    //    }
-    //}
-
-    // Corrects Y flip after wander method and before die method (Edge Cases)
-    //private void correctFlip()
-    //{
-    //    Vector3 localScale = transform.localScale;
-
-    //    if (upFlipped)
-    //    {
-    //        localScale.y *= -1;
-    //        transform.localScale = localScale;
-    //        upFlipped = false;
-    //    }
-    //}
-
-    // Updates facing direction of creature based on velocity
-    //private void facingUpdate()
-    //{
-    //    // Velocity Based Direction
-    //    Vector2 targetPosition = target.position;
-    //    Vector2 currentPosition = transform.position;
-
-    //    Vector2 direction = (targetPosition - currentPosition).normalized;
-
-    //    transform.right = myRigidbody.velocity;
-
-    //    float angle;
-    //    float rotateSpeed = 2;
-
-    //    angle = Mathf.Sign(Vector2.SignedAngle(transform.up, myRigidbody.velocity));
-
-    //    // This is to stop overrotation
-    //    if (Mathf.Abs(Vector2.Angle(transform.right, myRigidbody.velocity)) < 5f)
-    //    {
-    //        angle = 0;
-    //    }
-
-    //    if (angle != 0)
-    //    {
-    //        myRigidbody.MoveRotation(myRigidbody.rotation + rotateSpeed * angle * Time.fixedDeltaTime);
-    //    }
-    //}
-
-
-    // Status Checks
-    //
-    //
     // Checking if enemy hit player or Torpedo
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Nightingale" && !hitPlayer && !hitByTorpedo)
+        if (collision.gameObject.tag == "Enemy")
         {
-            hitPlayer = true;
+            grabCreature = true;
         }
     }
 
     // Checks if player is in range
-    private bool IsPlayerInRange(float range)
+    private bool isPlayerInRange(float range)
     {
         return Vector3.Distance(transform.position, player.transform.position) <= range;
     }
+
+    // Checks if a creature is in range
+    private bool isCreatureInRange(float range)
+    {
+        //Need Charging Fish
+        Vector3 enemyPosition = GameObject.FindGameObjectWithTag("SwordFish").transform.position;
+        return Vector3.Distance(transform.position, enemyPosition) <= range;
+    }
+
+    IEnumerator jumpscareActivate()
+    {
+        animator.SetBool("Jumpscare", true);
+        yield return new WaitForSeconds(0.3f);
+        jumpscare = false;
+        animationClear();
+        yield return new WaitForSeconds(1);
+        myRigidbody.velocity = new Vector2(0f, 0f);
+    }
+
+    IEnumerator attackActivate()
+    {
+        if(attacking)
+        {
+            attacking = false;
+            animator.SetBool("Attack", true);
+            myRigidbody.velocity = new Vector2(-4f, 0f);
+            yield return new WaitForSeconds(0.1f);
+            animationClear();
+            yield return new WaitForSeconds(0.5f);
+            myRigidbody.velocity = new Vector2(2f, 0f);
+            yield return new WaitForSeconds(1);
+            myRigidbody.velocity = new Vector2(0f, 0f);
+            attacking = true;
+        }
+    }
+
+    IEnumerator Leaving()
+    {
+        if (leaveTrack)
+        {
+            leaveTrack = false;
+            //attack part
+            animator.SetBool("Attack", true);
+            myRigidbody.velocity = new Vector2(-4f, 0f);
+            yield return new WaitForSeconds(0.1f);
+            animationClear();
+            yield return new WaitForSeconds(0.5f);
+            myRigidbody.velocity = new Vector2(2f, 0f);
+            yield return new WaitForSeconds(1);
+            myRigidbody.velocity = new Vector2(0f, 0f);
+
+
+            //leaving part
+            animator.SetBool("grabFish", true);
+            myRigidbody.velocity = new Vector2(0f, -1f);
+            animator.SetBool("Attack", true);
+            yield return new WaitForSeconds(0f);
+            animator.SetBool("Attack", false);
+            yield return new WaitForSeconds(1);
+            myRigidbody.velocity = new Vector2(0f, 0f);
+        }
+        
+    }
+
+    private void animationClear()
+    {
+        animator.SetBool("Attack", false);
+        animator.SetBool("grabFish", false);
+        animator.SetBool("Jumpscare", false);
+    }
 }
-
-
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class Monstrosquid : MonoBehaviour
-//{
-//    private Animator myAnimator;
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        myAnimator = GetComponent<Animator>();
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-
-//        myAnimator.SetTrigger(name: "jumpScare");
-//    }
-//}
---
+++ Assets/Scripts/Creature/SwordFish.cs
@@ -0,0 +1,332 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UIElements.Experimental;
+
+public class SwordFish : MonoBehaviour
+{
+    public enum EnemyAction
+    {
+        Wander,
+        Prime,
+        Dash,
+        Die,
+    }
+
+    private GameObject player;
+    public EnemyAction currState = EnemyAction.Wander;
+
+    public Transform target;
+    Rigidbody2D myRigidbody;
+    
+    // Editable movement variables
+    public float range = 5f;
+    public float moveSpeed = 2f;
+    public float dashSpeed = 8f;
+    public float dashChargeTimer = 3f;
+    public float dashCooldownTimer = 7f;
+    private float turnInterval = 5.0f;
+    private float slowTimeInterval = 0.5f;
+    private Vector2 oneDirection;
+
+    // Bool's for creature state changes
+    private bool hitPlayer = false;
+    private bool hitByTorpedo = false;
+    private bool isFacingRight = true;
+    private bool creatureTurn = false;
+    private bool upFlipped = false;
+    private bool slowTimeActive = false;
+    private bool slowTimeCancel = false;
+    public bool dashPrimed = false;
+    public bool dashOnCooldown = true;
+    private bool directionTaken = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        myRigidbody = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        StartCoroutine(ChangeCreatureTurn());
+        StartCoroutine(DashCooldown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateSwitch();
+
+        if (!slowTimeCancel)
+        {
+            FacingUpdate();
+        }
+    }
+    
+    // Switches the states of the enemy creature
+    private void stateSwitch()
+    {
+        // Checks what state to be in
+        if (hitByTorpedo)
+        {
+            currState = EnemyAction.Die;
+        }
+        else if (IsPlayerInRange(range) && dashPrimed && !dashOnCooldown && currState == EnemyAction.Prime)
+        {
+            currState = EnemyAction.Dash;
+        }
+        else if (IsPlayerInRange(range) && !dashOnCooldown)
+        {
+            currState = EnemyAction.Prime;
+        }
+        else
+        {
+            currState = EnemyAction.Wander;
+        }
+
+        // Switches current state based on currState
+        switch (currState)
+        {
+            case EnemyAction.Wander:
+                Wander();
+                break;
+            case EnemyAction.Prime:
+                Prime();
+                break;
+            case EnemyAction.Dash:
+                Dash();
+                break;
+            case EnemyAction.Die:
+                Die();
+                break;
+        }
+    }
+
+    // Different creature state methods
+    //
+    //
+    // Wander creature state
+    void Wander()
+    {
+        directionTaken = false;
+        Vector3 localScale = transform.localScale;
+        
+        if (transform.up.y < 0f)
+        {
+            localScale.y = 1;
+            transform.localScale = localScale;
+            upFlipped = true;
+        }
+
+        if (creatureTurn)
+        {
+            myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else
+        {
+            myRigidbody.velocity = new Vector2(-moveSpeed, 0f);
+        }
+    }
+
+    // Follow creature state
+    void Prime()
+    {
+        directionTaken = false;
+        Vector2 targetPosition = target.position;
+        Vector2 currentPosition = transform.position;
+
+        Vector2 direction = (targetPosition - currentPosition).normalized;
+
+        transform.right = myRigidbody.velocity;
+
+        myRigidbody.velocity = new Vector2(direction.x * 0.1f, direction.y * 0.1f);
+
+        StartCoroutine(DashCharge());
+        StartCoroutine(DashCooldown());
+        Flip();
+        CorrectFlip();
+    }
+
+    // Run creature state
+    void Dash()
+    {
+        // Single direction at time of dash
+        OneDirection();
+        Vector2 direction = oneDirection;
+
+        myRigidbody.velocity = new Vector2(direction.x * dashSpeed, direction.y * dashSpeed);
+    }
+
+    // Die method
+    void Die()
+    {
+        if (slowTimeActive == true)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x * 0.2f, myRigidbody.velocity.y * 0.2f);
+            slowTimeActive = false;
+        }
+    }
+
+    // Helper methods
+    //
+    //
+    // Flipping sprite at critical points (Looking Straight Up and Down)
+    private void Flip()
+    {
+        Vector2 targetPosition = target.position;
+        Vector2 currentPosition = transform.position;
+
+        Vector2 direction = (targetPosition - currentPosition).normalized;
+
+        if ((isFacingRight && direction.x < -0.01) || (!isFacingRight && direction.x > 0.01))
+        {
+            isFacingRight = !isFacingRight;
+
+            Vector3 localScale = transform.localScale;
+            localScale.y *= -1;
+            transform.localScale = localScale;
+        }
+    }
+
+    // Corrects Y flip after wander method and before die method (Edge Cases)
+    private void CorrectFlip()
+    {
+        Vector3 localScale = transform.localScale;
+
+        if (upFlipped)
+        {
+            localScale.y *= -1;
+            transform.localScale = localScale;
+            upFlipped = false;
+        }
+    }
+
+    // Updates facing direction of creature based on velocity
+    private void FacingUpdate()
+    {
+        // Velocity Based Direction
+        Vector2 targetPosition = target.position;
+        Vector2 currentPosition = transform.position;
+
+        Vector2 direction = (targetPosition - currentPosition).normalized;
+
+        transform.right = myRigidbody.velocity;
+
+        float angle;
+        float rotateSpeed = 2;
+
+        angle = Mathf.Sign(Vector2.SignedAngle(transform.up, myRigidbody.velocity));
+
+        // This is to stop overrotation
+        if (Mathf.Abs(Vector2.Angle(transform.right, myRigidbody.velocity)) < 5f)
+        {
+            angle = 0;
+        }
+
+        if (angle != 0)
+        {
+            myRigidbody.MoveRotation(myRigidbody.rotation + rotateSpeed * angle * Time.fixedDeltaTime);
+        }
+    }
+
+
+    // Status Checks
+    //
+    //
+    // Checking if enemy hit player or Torpedo
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Nightingale" && !hitPlayer && !hitByTorpedo)
+        {
+            hitPlayer = true;
+            myRigidbody.velocity = new Vector2(0, 0);
+            Vector3 localScale = transform.localScale;
+            transform.localScale = localScale;
+            StartCoroutine(DashCooldown());
+        }
+        else if (collision.gameObject.name == "Torpedo2(Clone)" && hitByTorpedo == false)
+        {
+            hitByTorpedo = true;
+            myRigidbody.bodyType = RigidbodyType2D.Dynamic;
+            myRigidbody.gravityScale = 0.01f;
+            StartCoroutine(deathSlowTime());
+            StartCoroutine(deathSlowTimeCancel());
+            CorrectFlip();
+        }
+        else if (collision.gameObject.name == "Monstrosquid")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            StartCoroutine(DashCooldown());
+        }
+
+    }
+
+    // Gets one direction
+    private void OneDirection()
+    {
+        if (!directionTaken)
+        {
+            Vector2 targetPosition = target.position;
+            Vector2 currentPosition = transform.position;
+
+            oneDirection = (targetPosition - currentPosition).normalized;
+            directionTaken = true;
+        }
+    }
+
+    // Checks if player is in range
+    private bool IsPlayerInRange(float range)
+    {
+        return Vector3.Distance(transform.position, player.transform.position) <= range;
+    }
+
+    // Changing Creature Momentum Direction
+    IEnumerator ChangeCreatureTurn()
+    {
+
+        Vector3 localScale = transform.localScale;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(turnInterval);
+            creatureTurn = !creatureTurn;
+            localScale.x *= -1f;
+        }
+    }
+
+    // Buffer timer so creature gets further away
+    IEnumerator DashCharge()
+    {
+        yield return new WaitForSeconds(dashChargeTimer);
+        dashPrimed = true;
+    }
+
+    IEnumerator DashCooldown()
+    {
+        yield return new WaitForSeconds(dashCooldownTimer);
+        dashOnCooldown = false;
+    }
+
+    // Time to slow down after dying
+    IEnumerator deathSlowTime()
+    {
+        if (!slowTimeCancel)
+        {
+            yield return new WaitForSeconds(slowTimeInterval);
+            slowTimeActive = true;
+        }
+    }
+
+    //Cancels the slow time after a given time
+    IEnumerator deathSlowTimeCancel()
+    {
+        yield return new WaitForSeconds(4);
+        slowTimeCancel = true;
+    }
+}
