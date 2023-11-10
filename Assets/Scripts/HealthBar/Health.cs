d485284f3f1860e3aa8375700808078c3a115816
Leo Dresang <lrdresang@yahoo.com>
2023-11-10 13:16:09 -0600

    fwfawf

--- Assets/Scripts/HealthBar/Health.cs
+++ Assets/Scripts/HealthBar/Health.cs
@@ -44,7 +44,7 @@ public class Health : MonoBehaviour
         }
         if (currHealth < 1)
         {
-            deathSound.Play();
+            //deathSound.Play();
             onFail();
         }
     }
