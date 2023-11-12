1cad95bd02f7279bba4381bc8677fde462373aab
Leo Dresang <lrdresang@yahoo.com>
2023-11-12 00:05:24 -0600

    sefsef

--- Assets/Scripts/Nightingale/RotorSound.cs
+++ Assets/Scripts/Nightingale/RotorSound.cs
@@ -7,7 +7,6 @@ public class RotorSound : MonoBehaviour
     public AudioSource Sound;
     public GameObject Nightingale;
     private float speed;
-    private bool isSoundPlaying = false;
     void Start()
     {
         
@@ -22,39 +21,20 @@ public class RotorSound : MonoBehaviour
         }
 
         speed = Nightingale.GetComponent<NightingaleMovement>().getVelocity().magnitude;
+
+ 
         
-        if((speed > 0.3) && !isSoundPlaying)
+        if(speed > 0.3)
         {
-            StartCoroutine(playSound());
+            Sound.pitch = (speed / 5f) + 0.4f;
+            Sound.volume = (speed / 30f) + 0.05f;
+        }
+        else{
+            Sound.pitch = 1;
+            Sound.volume = 0;
         }
 
-        Debug.Log(speed);
 
     }
 
-    IEnumerator playSound()
-    {
-        isSoundPlaying = true;
-
-        Sound.Play();
-
-
-        for (int i = 0; i < 100; i++)
-        {
-            Sound.pitch = (speed / 4f) + 0.3f;
-            Sound.volume = (speed / 20f) + 0.3f;
-
-            yield return new WaitForSeconds(0.01f);
-            if (speed < 0.3)
-            {
-                break;
-            }
-        }
-
-        Sound.Stop();
-
-        isSoundPlaying = false;
-
-        yield return null;
-    }
 }
