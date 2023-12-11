577b61958b3d0455e6dad5735384b45f196406c7
RyanG12143 <loop9981@gmail.com>
2023-12-11 15:17:09 -0600

    po

---
+++ Assets/Scripts/UI/FadeIn.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+
+    public float fadeSpeed;
+    public GameObject blackScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FadeFromBlack());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator FadeFromBlack()
+    {
+        blackScreen.SetActive(true);
+
+        Color prevObjectColor = Color.black;
+        prevObjectColor = new Color(prevObjectColor.r, prevObjectColor.g, prevObjectColor.b, 1);
+        Color objectColor = Color.black;
+        objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, 0);
+        float fadeAmount = 0;
+
+        while (blackScreen.GetComponent<Image>().color.a > .8)
+        {
+            fadeAmount = fadeAmount + (fadeSpeed / 4 * Time.deltaTime);
+            blackScreen.GetComponent<Image>().color = Color.Lerp(prevObjectColor, objectColor, fadeAmount);
+            yield return null;
+        }
+        while (blackScreen.GetComponent<Image>().color.a > .5)
+        {
+            fadeAmount = fadeAmount + (fadeSpeed / 2 * Time.deltaTime);
+            blackScreen.GetComponent<Image>().color = Color.Lerp(prevObjectColor, objectColor, fadeAmount);
+            yield return null;
+        }
+        while (blackScreen.GetComponent<Image>().color.a > 0)
+        {
+            fadeAmount = fadeAmount + (fadeSpeed * Time.deltaTime);
+            blackScreen.GetComponent<Image>().color = Color.Lerp(prevObjectColor, objectColor, fadeAmount);
+            yield return null;
+        }
+
+        blackScreen.SetActive(false);
+        yield return null;
+    }
+}
